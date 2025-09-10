610fc56346cfbd79f8755e5f4cba85ccee4a0c6e
Wintermute84 <neuromancerr83@gmail.com>
2025-09-10 22:43:00 +0530

    view rooms functionalities added
    added assets, styles acc to final mockup

--- exam_allocation/index.php
+++ exam_allocation/index.php
@@ -1,3 +1,43 @@
-<?php
-echo "Exam Seating Allocation Project Setup Successful!";
-?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Exam Allocation</title>
+    <link rel="stylesheet" href="./styles/output.css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Geist:wght@100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+  </head>
+  <body class="bg-black min-h-screen flex flex-col">
+
+    <header class="border-b-2 h-[100px] border-[#FFFFFF] flex items-center">
+      <div class="w-[25px] h-[25px] bg-[#9E9B9B] border-3 rounded-sm border-[#FFFEFE] ml-3">
+      </div>
+    </header>
+    
+    <section class="flex-1 flex items-center justify-evenly">
+      <div class="text-white max-w-[50%] text-center flex flex-col items-center justify-evenly">
+        <h1 class="font-medium text-4xl mb-[54px]">The Foundation for all your seating requirements</h1>
+        <h4 class="text-2xl">An all in one solution to fulfil all your seating requirements at without all the hassle</h4>
+      </div>
+      <div class="w-[480px] h-[550px] p-3 bg-[#0A0A0A] border-2 border-[#474444] rounded-3xl">
+        <form class="flex flex-col justify-evenly h-[100%]">
+          <div class="ml-6 mt-5">
+            <h2 class="text-3xl">Sign In</h2>
+            <h6 class="text-white secondary">Enter your credentials to sign in</h6>
+          </div>
+          <label class="ml-6 flex flex-col mb-5">
+            Username
+            <input type="text" class="border-2 border-[#605F5F] px-3 rounded-[15px] h-[50px] w-[80%] bg-[#2E2E2E]" placeholder="john doe">
+          </label>
+          <label class="ml-6 flex flex-col mb-5">
+            Password
+            <input type="password" class="border-2 border-[#605F5F] px-3 rounded-[15px] h-[50px] w-[80%] bg-[#2E2E2E]" >
+          </label>
+          <button type="button" class="bg-[#E5E5E5] text-lg w-[200px] mx-auto p-3 rounded-[10px]">Sign In</button>
+        </form>
+      </div>
+    </section>
+  
+  </body>
+</html>
+
--- exam_allocation/view_rooms.php
+++ exam_allocation/view_rooms.php
@@ -6,69 +6,133 @@ $result = $conn->query($sql);
 ?>
 
 <!DOCTYPE html>
-<html>
-<head>
+<html lang="en">
+  
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>View Rooms</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            margin: 30px;
-        }
-        table {
-            border-collapse: collapse;
-            width: 80%;
-        }
-        th, td {
-            padding: 10px;
-            border: 1px solid #ccc;
-            text-align: center;
-        }
-        th {
-            background: #f2f2f2;
-        }
-        h2 {
-            margin-bottom: 20px;
-        }
-        a {
-            display: inline-block;
-            margin-bottom: 20px;
-            text-decoration: none;
-            background: #007bff;
-            color: white;
-            padding: 8px 12px;
-            border-radius: 4px;
-        }
-        a:hover {
-            background: #0056b3;
-        }
-    </style>
-</head>
-<body>
-    <h2>Room List</h2>
-    <a href="upload_rooms.php">➕ Upload New CSV</a>
+    <link rel="stylesheet" href="./styles/output.css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Geist:wght@100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./styles/output.css">
+  </head>
 
-    <table>
-        <tr>
-            <th>RID</th>
-            <th>Block</th>
-            <th>Room Number</th>
-            <th>Capacity</th>
-            <th>Type</th>
-        </tr>
+  <body class="bg-black h-screen flex flex-col relative">
 
-        <?php if ($result->num_rows > 0): ?>
+    <header class="border-b-2 h-[100px] border-[#FFFFFF] flex relative">
+      <div class="flex items-center justify-between w-full">
+        <div class="w-[25px] h-[25px] bg-[#9E9B9B] border-3 rounded-sm border-[#FFFEFE] ml-3"></div>  
+        <div class="relative group inline-block">
+          <div class="cursor-pointer w-[25px] h-[25px] bg-gradient-to-b from-[#18C088] via-[#10855E] via-70% via-[#0D6D4D] to-[#0B5A40] rounded-xl border-2 border-[#828282] mr-3"></div>
+          <div class="absolute top-6 right-1 opacity-0 translate-y-2 pointer-events-none group-hover:opacity-100 group-hover:translate-y-0 group-hover:pointer-events-auto transition-all duration-300 ease-in-out h-9 w-[120px] bg-[#373737] border p-3 flex items-center rounded-md shadow-lg">
+            <div class="flex items-center justify-center w-full gap-2 hover:bg-[#5C5555] rounded-sm transition-colors duration-200 cursor-pointer select-none">
+              <img src="./assets/logout.png" alt="logout img">
+              <p class="text-sm">Log out</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="flex absolute -bottom-0 w-[70%] justify-evenly text-[18px] ml-5 select-none">
+        <img src="./assets/ham_menu.png" alt="hamburger menu" class="hidden">
+        <p class="secondary cursor-pointer">Overview</p>
+        <p class="secondary cursor-pointer">Seating Plan</p>
+        <p class="secondary cursor-pointer">Exams</p>
+        <p class="border-b-2 pb-1 cursor-pointer">Rooms</p>
+        <p class="secondary cursor-pointer">Students</p>
+        <p class="secondary cursor-pointer">Invigilation</p>
+        <p class="secondary cursor-pointer">Programmes</p>
+      </div>
+    </header>
+    
+    <main class="flex-1 flex">
+      <section class="relative flex-1 flex items-center justify-center">
+        <div class=" absolute bg-white w-[0.5px] h-[95%] right-0"></div>
+        <div class="w-[60%] h-[60%] grid grid-rows-5 grid-flow-col ">
+          <div class="w-[35px] h-[35px] bg-[#9E9B9B] border-3 rounded-md border-[#FFFEFE] ml-3"></div>  
+          <div class="w-[35px] h-[35px] bg-[#9E9B9B] border-3 rounded-md border-[#FFFEFE] ml-3"></div>  
+          <div class="w-[35px] h-[35px] bg-[#9E9B9B] border-3 rounded-md border-[#FFFEFE] ml-3"></div>  
+          <div class="w-[35px] h-[35px] bg-[#9E9B9B] border-3 rounded-md border-[#FFFEFE] ml-3"></div>  
+          <div class="w-[35px] h-[35px] bg-[#9E9B9B] border-3 rounded-md border-[#FFFEFE] ml-3"></div>  
+          <div class="w-[35px] h-[35px] bg-[#9E9B9B] border-3 rounded-md border-[#FFFEFE] ml-3"></div>  
+          <div class="w-[35px] h-[35px] bg-[#9E9B9B] border-3 rounded-md border-[#FFFEFE] ml-3"></div>  
+          <div class="w-[35px] h-[35px] bg-[#9E9B9B] border-3 rounded-md border-[#FFFEFE] ml-3"></div>  
+          <div class="w-[35px] h-[35px] bg-[#9E9B9B] border-3 rounded-md border-[#FFFEFE] ml-3"></div>  
+          <div class="w-[35px] h-[35px] bg-[#9E9B9B] border-3 rounded-md border-[#FFFEFE] ml-3"></div>  
+          <div class="w-[35px] h-[35px] bg-[#9E9B9B] border-3 rounded-md border-[#FFFEFE] ml-3"></div>  
+          <div class="w-[35px] h-[35px] bg-[#9E9B9B] border-3 rounded-md border-[#FFFEFE] ml-3"></div>  
+          <div class="w-[35px] h-[35px] bg-[#9E9B9B] border-3 rounded-md border-[#FFFEFE] ml-3"></div>  
+          <div class="w-[35px] h-[35px] bg-[#9E9B9B] border-3 rounded-md border-[#FFFEFE] ml-3"></div>  
+          <div class="w-[35px] h-[35px] bg-[#9E9B9B] border-3 rounded-md border-[#FFFEFE] ml-3"></div>  
+          <div class="w-[35px] h-[35px] bg-[#9E9B9B] border-3 rounded-md border-[#FFFEFE] ml-3"></div>  
+          <div class="w-[35px] h-[35px] bg-[#9E9B9B] border-3 rounded-md border-[#FFFEFE] ml-3"></div>  
+          <div class="w-[35px] h-[35px] bg-[#9E9B9B] border-3 rounded-md border-[#FFFEFE] ml-3"></div>  
+          <div class="w-[35px] h-[35px] bg-[#9E9B9B] border-3 rounded-md border-[#FFFEFE] ml-3"></div>  
+          <div class="w-[35px] h-[35px] bg-[#9E9B9B] border-3 rounded-md border-[#FFFEFE] ml-3"></div>  
+          <div class="w-[35px] h-[35px] bg-[#9E9B9B] border-3 rounded-md border-[#FFFEFE] ml-3"></div>  
+          <div class="w-[35px] h-[35px] bg-[#9E9B9B] border-3 rounded-md border-[#FFFEFE] ml-3"></div>  
+          <div class="w-[35px] h-[35px] bg-[#9E9B9B] border-3 rounded-md border-[#FFFEFE] ml-3"></div>  
+          <div class="w-[35px] h-[35px] bg-[#9E9B9B] border-3 rounded-md border-[#FFFEFE] ml-3"></div>  
+          <div class="w-[35px] h-[35px] bg-[#9E9B9B] border-3 rounded-md border-[#FFFEFE] ml-3"></div>  
+          <div class="w-[35px] h-[35px] bg-[#9E9B9B] border-3 rounded-md border-[#FFFEFE] ml-3"></div>  
+          <div class="w-[35px] h-[35px] bg-[#9E9B9B] border-3 rounded-md border-[#FFFEFE] ml-3"></div>  
+          <div class="w-[35px] h-[35px] bg-[#9E9B9B] border-3 rounded-md border-[#FFFEFE] ml-3"></div>  
+          <div class="w-[35px] h-[35px] bg-[#9E9B9B] border-3 rounded-md border-[#FFFEFE] ml-3"></div>  
+          <div class="w-[35px] h-[35px] bg-[#9E9B9B] border-3 rounded-md border-[#FFFEFE] ml-3"></div>  
+        </div>
+      </section>
+      <section class="flex-1 flex items-start justify-center mt-5">
+        <div  class="w-[80%] h-[80%]">
+          <div x-data="{ open: false }" class="flex items-center justify-between m-2">
+            <a href="upload_rooms.php"><button class="h-[50px] w-[200px] bg-[#E5E5E5] rounded-sm cursor-pointer">Upload CSV</button></a>
+            <div @click="open = !open"  
+                class="flex items-center justify-between relative border h-[35px] w-[200px] px-2 rounded-md bg-[#373737] cursor-pointer">
+              <p>M George Block</p>
+              <img class="h-[16px]" src="assets/dropdown.png" alt="dropdown icon">
+              <div x-show="open"
+                  @click.outside="open = false" 
+                  x-transition:enter="transition ease-out duration-200"
+                  x-transition:enter-start="opacity-0 scale-95"
+                  x-transition:enter-end="opacity-100 scale-100"
+                  x-transition:leave="transition ease-in duration-150"
+                  x-transition:leave-start="opacity-100 scale-100"
+                  x-transition:leave-end="opacity-0 scale-95"
+                  class="bg-[#373737] absolute top-full mt-2 z-40 -left-10 border w-[238px] h-fit flex flex-col items-start p-3 rounded-md gap-1">
+                <p class="hover:bg-[#5C5555] w-full px-2 py-1 rounded-md">M George Block</p>
+                <p class="hover:bg-[#5C5555] w-full px-2 py-1 rounded-md">Rameshwaram Block</p>                              
+              </div>
+            </div>
+          </div>
+
+          <div class="flex flex-col w-full items-end mt-10 gap-2 overflow-auto h-[400px]">
+            <?php if ($result->num_rows > 0): ?>
             <?php while ($row = $result->fetch_assoc()): ?>
-                <tr>
-                    <td><?= $row['Rid'] ?></td>
-                    <td><?= $row['Block'] ?></td>
-                    <td><?= $row['Room_no'] ?></td>
-                    <td><?= $row['Capacity'] ?></td>
-                    <td><?= $row['Type'] ?></td>
-                </tr>
+              <div class="w-[80%] min-h-[80px] max-h-[85px] cursor-pointer bg-[#151515] mr-2 border rounded-sm flex items-center justify-between">
+                <div class="w-fit flex flex-col ml-2">
+                  <p class="text-sm">No - <?= $row['Room_no'] ?></p>
+                  <p class="text-sm">Capacity - <?= $row['Capacity'] ?></p>
+                  <p class="text-sm">Room Type - <?= $row['Type'] ?></p>
+                </div>
+                <div class="flex gap-2 mr-4">
+                  <button class="h-[35px] w-[35px] bg-white flex items-center justify-center border rounded-md">
+                    <img class="h-[20px]" src="./assets/delete.png" alt="delete icon">
+                  </button>
+                  <button class="h-[35px] w-[35px] bg-white flex items-center justify-center  border rounded-md">
+                    <img class="h-[20px]" src="./assets/edit.png" alt="edit icon">
+                  </button>
+                </div>
+              </div>    
             <?php endwhile; ?>
-        <?php else: ?>
-            <tr><td colspan="5">No rooms found.</td></tr>
-        <?php endif; ?>
-    </table>
-</body>
+            <?php else: ?>
+                <p>No rooms found.</p>
+            <?php endif; ?>
+          </div>
+        </div>
+      </section>
+    </main>  
+
+    <button class="absolute bg-white w-[50px] h-[50px] rounded-full flex items-center justify-center bottom-8 right-3 cursor-pointer"><img class="h-[25px]" src="assets/add.png" alt="add icon"></button>
+    <script src="https://unpkg.com/alpinejs" defer></script>
+  </body>
 </html>
